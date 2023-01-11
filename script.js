619b966ff87bf0c1e126ec32caff9b20e5de02b6
Chirine Laroussi <86356232+chirine2001@users.noreply.github.com>
2023-01-11 15:31:52 +0100

    random facts on page anecdotes

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+ function getCatFact() {
+    fetch("https://catfact.ninja/fact/")
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("catfact").innerHTML = data.fact;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
