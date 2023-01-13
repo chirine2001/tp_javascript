dc98f146289dd93de78afdafa1920f7670b6bdb0
Chirine Laroussi <86356232+chirine2001@users.noreply.github.com>
2023-01-13 15:37:56 +0100

    Page Races : Détails des races (affichage des races et de leur détails)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function darkMode() {
 	var element = document.body;
 	element.classList.toggle("dark-mode");
 }
@@ -20,21 +20,21 @@ fetch(queryUrl)
 		// on récupère le tableau 'data' du json
 		const messageData = data.data;
 		// on crée des ensembles utiliser plus tard pour éviter la duplication
-		const countrySet = new Set();
+		const breedSet = new Set();
 		const coatSet = new Set();
 		// on récupère les éléments html
-		const catCountryList = document.getElementById('select-country');
+		const catBreedList = document.getElementById('select-breed');
 		const catCoatList = document.getElementById('select-coat');
 		// on boucle sur les elements du tableau 'data' 
 		for (const element of messageData) {
-			if (!countrySet.has(element.country)) {
-				countrySet.add(element.country); 
-				// création de l'option du select avec le paramètre 'country' = pays
-				const licountry = document.createElement('option');
-				licountry.setAttribute('value', element.country);
-				licountry.innerText = element.country;
+			if (!breedSet.has(element.breed)) {
+				breedSet.add(element.breed);
+				// création de l'option du select avec le paramètre 'breed' = pays
+				const liBreed = document.createElement('option');
+				liBreed.setAttribute('value', element.breed);
+				liBreed.innerText = element.breed;
 				//ajout du pays comme option du select
-				catCountryList.appendChild(licountry);
+				catBreedList.appendChild(liBreed);
 			}
 			if (!coatSet.has(element.coat)) {
 				coatSet.add(element.coat);
@@ -48,8 +48,35 @@ fetch(queryUrl)
 		}
 	});
 
+function catFeatures() {
+	selectBreedElmt = document.getElementById("select-breed");
+	catBreedSelected = selectBreedElmt.options[selectBreedElmt.selectedIndex].value;
 
+	selectCoatElmt = document.getElementById("select-coat");
+	catCoatSelected = selectCoatElmt.options[selectCoatElmt.selectedIndex].value;
 
+	queryUrl = 'https://catfact.ninja/breeds';
+	fetch(queryUrl)
+		.then(response => response.json())
+		.then(data => {
+			const messageData = data.data;
+			const filteredBreedData = messageData.filter(element => element.breed === catBreedSelected);
+			if (selectBreedElmt.selectedIndex = 0) {
+				const filteredCoatData = filteredBreedData.filter(breed => breed.coat === catCoatSelected);
+				cat = filteredCoatData[0]
+			}else{
+				cat = filteredBreedData[0]
+			}
+
+			const explanationElement = document.getElementById('explain');
+
+			if (cat != null) {
+				explanationElement.innerText = 'Breed: ' + cat.breed + '\n Country of origin: ' + cat.country + ' \n Place of origin: ' + cat.origin + '\nCoat: ' + cat.coat + '\nPattern: ' + cat.pattern;
+			} else {
+				explanationElement.innerText = 'Nothing found for the breed ' + catBreedSelected + ' associated to the ' + catCoatSelected + ' coat';
+			}
+		});
+}
 
 
 
